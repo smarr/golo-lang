35a1b237930b65c98768bbe4b13949afffbdba7b
Stefan Marr <git@stefan-marr.de>
2015-07-01 13:40:52 +0200

    Added missing specializations
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/main/java/gololang/truffle/PlusNode.java
+++ src/main/java/gololang/truffle/PlusNode.java
@@ -8,4 +8,18 @@ public abstract class PlusNode extends BinaryNode {
   public long doLongs(final long left, final long right) {
     return left + right;
   }
+
+  @Specialization
+  public String doStrings(final String left, final String right) {
+    return left + right;
+  }
+
+  @Specialization
+  public String doLongAndString(final long left, final String right) {
+    return left + right;
+  }
+
+  public String doStringAndLong(final String left, final long right) {
+    return left + right;
+  }
 }
