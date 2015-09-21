f8e266e59bb8bd2c5b7fe988e5cd1a4421974e1d
Stefan Marr <git@stefan-marr.de>
2015-09-21 17:46:10 +0200

    Added ReturnException and ReturnNode
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/main/java/gololang/truffle/nodes/controlflow/ReturnException.java
@@ -0,0 +1,19 @@
+package gololang.truffle.nodes.controlflow;
+
+import com.oracle.truffle.api.nodes.ControlFlowException;
+
+
+public final class ReturnException extends ControlFlowException {
+
+  private static final long serialVersionUID = 7613567555163509250L;
+
+  private final Object result;
+
+  public ReturnException(final Object result) {
+    this.result = result;
+  }
+
+  public Object getResult() {
+    return result;
+  }
+}
---
+++ src/main/java/gololang/truffle/nodes/controlflow/ReturnNode.java
@@ -0,0 +1,20 @@
+package gololang.truffle.nodes.controlflow;
+
+import gololang.truffle.ExpressionNode;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+
+public final class ReturnNode extends ExpressionNode {
+
+  @Child protected ExpressionNode expr;
+
+  public ReturnNode(final ExpressionNode expr) {
+    this.expr = expr;
+  }
+
+  @Override
+  public Object executeGeneric(final VirtualFrame frame) {
+    throw new ReturnException(expr.executeGeneric(frame));
+  }
+}
