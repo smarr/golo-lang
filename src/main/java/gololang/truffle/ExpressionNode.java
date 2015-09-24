28e1d398e63397464a85126ffb1024e1dac44db9
Stefan Marr <git@stefan-marr.de>
2015-09-24 14:33:44 +0200

    Remove Class and Function from ExpressionNode/Types, not yet needed
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/main/java/gololang/truffle/ExpressionNode.java
+++ src/main/java/gololang/truffle/ExpressionNode.java
@@ -4,9 +4,6 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
-import fr.insalyon.citi.golo.compiler.parser.GoloParser.FunctionRef;
-import fr.insalyon.citi.golo.compiler.parser.GoloParser.ParserClassRef;
-
 public abstract class ExpressionNode extends Node {
   // TODO add constructor that takes SourceSection to simplify debugging
 
@@ -18,14 +15,6 @@ public abstract class ExpressionNode extends Node {
 
   public boolean executeBoolean(final VirtualFrame frame) throws UnexpectedResultException {
 	return TypesGen.expectBoolean(executeGeneric(frame));
-  }
-
-  public ParserClassRef executeParserClassRef(final VirtualFrame frame) throws UnexpectedResultException {
-	return TypesGen.expectParserClassRef(executeGeneric(frame));
-  }
-
-  public FunctionRef executeFunctionRef(final VirtualFrame frame) throws UnexpectedResultException {
-	return TypesGen.expectFunctionRef(executeGeneric(frame));
   }
 
   public Object[] executeObjectArray(final VirtualFrame frame) throws UnexpectedResultException {
--- src/main/java/gololang/truffle/Types.java
+++ src/main/java/gololang/truffle/Types.java
@@ -8,8 +8,6 @@ import fr.insalyon.citi.golo.compiler.parser.GoloParser;
 @TypeSystem({
   int.class,
   boolean.class,
-  GoloParser.ParserClassRef.class,
-  GoloParser.FunctionRef.class,
   Object[].class
 })
 public class Types { }
