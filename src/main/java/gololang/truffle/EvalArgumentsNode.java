8c2c1e7a0bb900e58975196f6e7a41da057b4f16
Stefan Marr <git@stefan-marr.de>
2015-09-24 15:20:58 +0200

    Added EvalArgumentsNode
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/main/java/gololang/truffle/EvalArgumentsNode.java
@@ -0,0 +1,33 @@
+package gololang.truffle;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
+
+
+public final class EvalArgumentsNode extends ExpressionNode {
+
+  @Children protected final ExpressionNode[] argumentNodes;
+
+  public EvalArgumentsNode(final ExpressionNode[] argumentNodes) {
+    this.argumentNodes = argumentNodes;
+  }
+
+  public ExpressionNode[] getArgumentNodes() {
+    return argumentNodes;
+  }
+
+  @Override
+  @ExplodeLoop
+  public Object[] executeObjectArray(final VirtualFrame frame) {
+    Object[] arguments = new Object[argumentNodes.length];
+    for (int i = 0; i < argumentNodes.length; i++) {
+      arguments[i] = argumentNodes[i].executeGeneric(frame);
+    }
+    return arguments;
+  }
+
+  @Override
+  public Object executeGeneric(final VirtualFrame frame) {
+    return executeObjectArray(frame);
+  }
+}
