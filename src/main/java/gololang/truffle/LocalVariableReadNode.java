bf0a0e1e9c1b2b1fe79d34a0f3440ca84e6d59b8
Stefan Marr <git@stefan-marr.de>
2015-10-09 15:59:43 +0200

    Don't need the uninitialized state.
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/main/java/gololang/truffle/LocalVariableReadNode.java
+++ src/main/java/gololang/truffle/LocalVariableReadNode.java
@@ -2,8 +2,6 @@ package gololang.truffle;
 
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.FrameSlot;
-import com.oracle.truffle.api.frame.FrameSlotKind;
-import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 
@@ -15,21 +13,8 @@ public abstract class LocalVariableReadNode extends ExpressionNode {
     this.slot = slot;
   }
 
-  @Specialization(guards = "isUninitialized()")
-  public Object doNull() {
-    return null;
-  }
-
-  @Specialization(guards = "isInitialized()", rewriteOn = {FrameSlotTypeException.class})
-  public Object doObject(final VirtualFrame frame) throws FrameSlotTypeException {
-    return frame.getObject(slot);
-  }
-
-  protected boolean isInitialized() {
-    return slot.getKind() != FrameSlotKind.Illegal;
-  }
-
-  protected boolean isUninitialized() {
-    return slot.getKind() == FrameSlotKind.Illegal;
+  @Specialization
+  public Object doObject(final VirtualFrame frame) {
+    return frame.getValue(slot);
   }
 }
